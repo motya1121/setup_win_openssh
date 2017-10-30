d4e258cf3812e861af20d0865383b0bf45fce192
motya0414 <motya0414@hotmail.co.jp>
2017-10-31 02:04:43 +0900

    update sshkey_setup_win.sh

--- sshkey_setup_win.sh
+++ sshkey_setup_win.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
 
-dir_exist=`ssh IEUser@192.168.0.4 "powershell Test-Path C:\Users\IEUser\.ssh"`
+vm_addr=$1
+vm_passwd=$2
+mstnode_account=$3
+
+
+dir_exist=`sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "powershell Test-Path C:\Users\IEUser\.ssh"`
 if [ $(echo "$dir_exist" | grep -e 'True') ]; then
-    echo ".ssh exist"
+    echo "## .ssh exist"
+    :
 else
-    echo ".ssh not exist. make .ssh"
-    ssh IEUser@192.168.0.4 "mkdir C:\Users\IEUser\.ssh"
+    echo "## .ssh not exist. make .ssh"
+    sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "mkdir C:\Users\IEUser\.ssh"
 fi
 
-dir_exist=`ssh IEUser@192.168.0.4 "powershell Test-Path C:\Users\IEUser\.ssh\authorized_keys"`
+
+dir_exist=`sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "powershell Test-Path C:\Users\IEUser\.ssh\authorized_keys"`
 if [ $(echo "$dir_exist" | grep -e 'True') ]; then
-    echo "authorized_keys exist"
+    echo "## authorized_keys exist"
 else
-    echo "authorized_keys not exist. make authorized_keys"
-    ssh IEUser@192.168.0.4 "type nul > C:\Users\IEUser\.ssh\authorized_keys"
+    echo "## authorized_keys not exist. make authorized_keys"
+    sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "type nul > C:\Users\IEUser\.ssh\authorized_keys"
 fi
 
-echo "add sshkey in authorized_keys"
+
+echo "## add sshkey in authorized_keys"
 sshkey=`cat ~/.ssh/id_rsa.pub`
-ssh IEUser@192.168.0.4 "echo ${sshkey} >> C:\Users\IEUser\.ssh\authorized_keys"
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "echo ${sshkey} >> C:\Users\IEUser\.ssh\authorized_keys"
 
-echo "create change acl script"
-ssh IEUser@192.168.0.4 "type nul > C:\Users\IEUser\.ssh\create_ch_acl.ps1"
-ssh IEUser@192.168.0.4 'echo $authorizedKeyPath = "C:\users\IEUser\.ssh\authorized_keys" >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
-ssh IEUser@192.168.0.4 'echo $acl = Get-Acl $authorizedKeyPath >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
-ssh IEUser@192.168.0.4 'echo $ar = New-Object System.Security.AccessControl.FileSystemAccessRule("NT Service\sshd", "Read", "Allow") >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
-ssh IEUser@192.168.0.4 'echo $acl.SetAccessRule($ar) >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
-ssh IEUser@192.168.0.4 'echo Set-Acl $authorizedKeyPath $acl >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
 
-echo "exe change acl script"
-ssh IEUser@192.168.0.4 "powershell C:\Users\IEUser\.ssh\create_ch_acl.ps1"
+echo "## create change acl script"
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "type nul > C:\Users\IEUser\.ssh\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} 'echo $authorizedKeyPath = "C:\users\IEUser\.ssh\authorized_keys" >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} 'echo $acl = Get-Acl $authorizedKeyPath >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} 'echo $ar = New-Object System.Security.AccessControl.FileSystemAccessRule("NT Service\sshd", "Read", "Allow") >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} 'echo $acl.SetAccessRule($ar) >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} 'echo Set-Acl $authorizedKeyPath $acl >> C:\Users\IEUser\.ssh\create_ch_acl.ps1'
 
-echo "delete acl script"
-ssh IEUser@192.168.0.4 "del /Q C:\Users\IEUser\.ssh\create_ch_acl.ps1"
+
+echo "## exe change acl script"
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "powershell C:\Users\IEUser\.ssh\create_ch_acl.ps1"
+
+echo "## delete acl script"
+sshpass -p ${vm_passwd} ssh IEUser@${vm_addr} "del /Q C:\Users\IEUser\.ssh\create_ch_acl.ps1"
